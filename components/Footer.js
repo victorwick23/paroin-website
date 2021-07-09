22a01cd3e2f09aaa9b67d75e52ab111e722e2525
victor <victor.dsdevs@gmail.com>
2021-07-10 04:33:42 +0700

    fix error

--- components/Footer.js
+++ components/Footer.js
@@ -12,10 +12,10 @@ export default function Footer() {
               <div className="flex-row">
                 <p className="text-white font-semibold">Social Media</p>
                 <div className="flex mt-2">
-                  <a className="pr-4" href="/">
+                  <a className="pr-4" href="www.instagram.com">
                       <img className="w-6" src="instagram.svg" alt="" />
                   </a>
-                  <a href="">
+                  <a href="www.instagram.com">
                       <img className="w-6" src="twitter.svg" alt="" />
                   </a>
                 </div>
@@ -23,7 +23,7 @@ export default function Footer() {
               <div className="flex-row mt-8">
                 <p className="text-white font-semibold">Customer Service</p>
                 <div className="flex mt-1">
-                  <a className="text-white" href="/">
+                  <a className="text-white" href="www.instagram.com">
                       +62 838-6675-4428
                   </a>
                 </div>
