1e9b720cbcdd6c2a2b87cfc95d9325b735f3eee5
victor <victor.dsdevs@gmail.com>
2021-07-21 14:09:03 +0700

    change preview

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
               <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;600;700&display=swap" rel="stylesheet"/>
               <link rel="icon" href="/favicon.png" />
-              <meta property="og:image" href="/preview.jpg"/>
+              <meta property="og:image" content="https://www.paroin.id/preview.jpg"/>
                
           </Head>
       
